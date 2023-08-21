493ff2142a96a916bbf9c1b22e1fdc2ddff92b54
CJH3213 <1603571877@qq.com>
2023-08-21 17:41:39 +0800

    First

---
+++ NAudioTest/NAudioTest/Form1.Designer.cs
@@ -0,0 +1,161 @@
+﻿namespace NAudioTest
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea5 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend5 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series5 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea6 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend6 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series6 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.chart_FFT = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.chart_Wave = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label_Info = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.chart_FFT)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart_Wave)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // chart_FFT
+            // 
+            chartArea5.AxisX.Minimum = 0D;
+            chartArea5.AxisY.Maximum = 200D;
+            chartArea5.AxisY.Minimum = 0D;
+            chartArea5.Name = "ChartArea1";
+            this.chart_FFT.ChartAreas.Add(chartArea5);
+            this.chart_FFT.Dock = System.Windows.Forms.DockStyle.Fill;
+            legend5.Name = "Legend1";
+            this.chart_FFT.Legends.Add(legend5);
+            this.chart_FFT.Location = new System.Drawing.Point(0, 3);
+            this.chart_FFT.Name = "chart_FFT";
+            series5.ChartArea = "ChartArea1";
+            series5.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series5.Legend = "Legend1";
+            series5.Name = "Series1";
+            this.chart_FFT.Series.Add(series5);
+            this.chart_FFT.Size = new System.Drawing.Size(992, 307);
+            this.chart_FFT.TabIndex = 0;
+            this.chart_FFT.Text = "chart1";
+            // 
+            // chart_Wave
+            // 
+            chartArea6.Name = "ChartArea1";
+            this.chart_Wave.ChartAreas.Add(chartArea6);
+            this.chart_Wave.Dock = System.Windows.Forms.DockStyle.Fill;
+            legend6.Enabled = false;
+            legend6.Name = "Legend1";
+            this.chart_Wave.Legends.Add(legend6);
+            this.chart_Wave.Location = new System.Drawing.Point(0, 0);
+            this.chart_Wave.Name = "chart_Wave";
+            series6.ChartArea = "ChartArea1";
+            series6.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series6.Legend = "Legend1";
+            series6.Name = "Series1";
+            this.chart_Wave.Series.Add(series6);
+            this.chart_Wave.Size = new System.Drawing.Size(992, 206);
+            this.chart_Wave.TabIndex = 1;
+            this.chart_Wave.Text = "chart1";
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.splitContainer1.Location = new System.Drawing.Point(5, 5);
+            this.splitContainer1.Name = "splitContainer1";
+            this.splitContainer1.Orientation = System.Windows.Forms.Orientation.Horizontal;
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.chart_Wave);
+            this.splitContainer1.Panel1.Padding = new System.Windows.Forms.Padding(0, 0, 0, 3);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.chart_FFT);
+            this.splitContainer1.Panel2.Padding = new System.Windows.Forms.Padding(0, 3, 0, 0);
+            this.splitContainer1.Size = new System.Drawing.Size(992, 523);
+            this.splitContainer1.SplitterDistance = 209;
+            this.splitContainer1.TabIndex = 2;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.label_Info);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel1.Location = new System.Drawing.Point(5, 534);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(992, 24);
+            this.panel1.TabIndex = 3;
+            // 
+            // label_Info
+            // 
+            this.label_Info.AutoSize = true;
+            this.label_Info.Font = new System.Drawing.Font("宋体", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label_Info.Location = new System.Drawing.Point(3, 4);
+            this.label_Info.Name = "label_Info";
+            this.label_Info.Size = new System.Drawing.Size(55, 16);
+            this.label_Info.TabIndex = 0;
+            this.label_Info.Text = "label1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1002, 563);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.splitContainer1);
+            this.Name = "Form1";
+            this.Padding = new System.Windows.Forms.Padding(5);
+            this.Text = "监听音频FFT";
+            ((System.ComponentModel.ISupportInitialize)(this.chart_FFT)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart_Wave)).EndInit();
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart_FFT;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart_Wave;
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label_Info;
+    }
+}
+
---
+++ NAudioTest/NAudioTest/Form1.cs
@@ -0,0 +1,171 @@
+﻿using NAudio.Dsp;
+using NAudio.Wave;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace NAudioTest
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            StartRecording();
+
+        }
+
+        // 更新音频波形曲线图
+        public void UpdateWaveChart(float[] d)
+        {
+
+            if (chart_Wave.InvokeRequired == false)
+            {
+                chart_Wave.Series["Series1"].Points.Clear();
+                for (int i = 0; i < d.Length; i++)
+                {
+                    chart_Wave.Series["Series1"].Points.AddXY(i, d[i]);
+                }
+            }
+            else
+            {
+                Action<float[]> action = UpdateWaveChart;
+                this.Invoke(action, new object[] { d});
+            }
+        }
+
+        // 更新FFT曲线图
+        public void UpdateFFTChart(double[] d, double baseFreq)
+        {
+
+            if(chart_FFT.InvokeRequired == false)
+            {
+                chart_FFT.Series["Series1"].Points.Clear();
+                for(int i = 0; i < d.Length; i++) 
+                {
+                    chart_FFT.Series["Series1"].Points.AddXY(i* baseFreq, d[i]);            
+                }
+
+                Console.WriteLine("{0}, {1}; ", d.Length, baseFreq);
+                int maxFreqIndex = MaxIndex(d);
+                Console.WriteLine("最大值：{0}, {1}; ", maxFreqIndex * baseFreq, d[maxFreqIndex]);
+                label_Info.Text = String.Format("最大值：频率{0:N3}, 幅度{1:N3}; ", maxFreqIndex * baseFreq, d[maxFreqIndex])
+                    + String.Format("FFT参数：点数{0}, 基频{1};", d.Length, baseFreq);
+            }
+            else
+            {
+                Action<double[], double> action = UpdateFFTChart;
+                this.Invoke(action, new object[] {d, baseFreq});
+            }
+        }
+
+
+
+        // 开始录制输出音频
+        private void StartRecording()
+        {
+            WasapiLoopbackCapture cap = new WasapiLoopbackCapture();
+            cap.DataAvailable += (sender, e) =>      // 录制数据可用时触发此事件, 参数中包含音频数据
+            {
+                float[] allSamples = Enumerable      // 提取数据中的采样
+                    .Range(0, e.BytesRecorded / 4)   // 除以四是因为, 缓冲区内每 4 个字节构成一个浮点数, 一个浮点数是一个采样
+                    .Select(i => BitConverter.ToSingle(e.Buffer, i * 4))  // 转换为 float
+                    .ToArray();    // 转换为数组
+
+                // 获取采样后, 在这里进行详细处理
+                var channelSamples = SplitChannels(allSamples, cap.WaveFormat.Channels);
+                var averageSamples = AverageChannels(channelSamples, cap.WaveFormat.Channels);
+                var fftResult = FFT(averageSamples);
+
+                double[] fftAbs = Enumerable.Range(0, fftResult.Length / 2)
+                .Select(i => {
+                    Complex c = fftResult[i]; 
+                    return Math.Sqrt(c.X * c.X + c.Y * c.Y); 
+                })
+                .ToArray();
+
+                // 计算FFT[1]的基础频率
+                double baseFreq = (double)cap.WaveFormat.SampleRate / fftResult.Length;
+
+                UpdateWaveChart(averageSamples);
+                UpdateFFTChart(fftAbs, baseFreq);
+            };
+            cap.StartRecording();   // 开始录制
+
+        }
+
+        // 分离声道
+        private float[][] SplitChannels(float[] allSamples, int channelCount)
+        {
+            // 设定我们已经将刚刚的采样保存到了变量 AllSamples 中, 类型为 float[]
+            // int channelCount = cap.WaveFormat.Channels;   // WasapiLoopbackCapture 的 WaveFormat 指定了当前声音的波形格式, 其中包含就通道数
+            float[][] channelSamples = Enumerable
+                .Range(0, channelCount)
+                .Select(channel => Enumerable
+                    .Range(0, allSamples.Length / channelCount)
+                    .Select(i => allSamples[channel + i * channelCount])
+                    .ToArray())
+                .ToArray();
+
+            return channelSamples;
+        }
+
+        // 对声道求均值
+        private float[] AverageChannels(float[][] channelSamples, int channelCount)
+        {
+            // 设定我们已经将分开了的采样保存到了变量 ChannelSamples 中, 类型为 float[][]
+            // 例如通道数为2, 那么左声道的采样为 ChannelSamples[0], 右声道为 ChannelSamples[1]
+            float[] averageSamples = Enumerable
+                .Range(0, channelSamples[0].Length)
+                .Select(index => Enumerable
+                    .Range(0, channelCount)
+                    .Select(channel => channelSamples[channel][index])
+                    .Average())
+                .ToArray();
+
+            return averageSamples;
+        }
+
+        // 傅里叶变换
+        private Complex[] FFT(float[] samples)
+        {
+            // 我们将对 AverageSamples 进行傅里叶变换, 得到一个复数数组
+
+            // 因为对于快速傅里叶变换算法, 需要数据长度为 2 的 n 次方, 这里进行
+            int log = (int)Math.Ceiling(Math.Log(samples.Length, 2));   // 取对数并向上取整
+            int newLen = (int)Math.Pow(2, log);                             // 计算新长度
+            float[] filledSamples = new float[newLen];
+            samples.CopyTo(filledSamples, 0);  // 拷贝到新数组
+            Complex[] complexSrc = filledSamples
+                .Select(v => new Complex() { X = v })        // 将采样转换为复数
+                .ToArray();
+            FastFourierTransform.FFT(false, log, complexSrc);   // 进行傅里叶变换
+
+            // 变换之后, complexSrc 则已经被处理过, 其中存储了频域信息
+            return complexSrc;
+        }
+
+        // 找出数组最大值下标
+        private int MaxIndex<T>(T[] arr) where T : IComparable
+        {
+            int maxIndex = 0;
+            T max = arr[0];
+            for(int i=1; i<arr.Length; i++)
+            {
+                if (arr[i].CompareTo(max) > 0)
+                {
+                    max = arr[i];
+                    maxIndex = i;
+                }
+            }
+            return maxIndex;
+        }
+    }
+}
